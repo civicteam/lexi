ebece60d7a0e140cd6e85256520ef9977bdd1773
dankelleher <dankelleher@yahoo.com>
2022-04-12 07:36:57 +0200

    Browser example

--- packages/lexi/src/lib/key.ts
+++ packages/lexi/src/lib/key.ts
@@ -1,21 +1,20 @@
 import {BoxKeyPair, randomBytes, secretbox, sign, SignKeyPair} from "tweetnacl";
-import {decodeBase64, encodeBase64} from "tweetnacl-util";
-import crypto from "crypto";
 import {convertKeyPair} from "ed2curve-esm";
 import type {SignWallet} from "./wallet";
+import * as crypto from "crypto";
+import * as base64 from "@stablelib/base64";
 
 const PUBLIC_STRING_LENGTH = 32;
 export const newNonce = () => randomBytes(secretbox.nonceLength);
 
-
 // Generate a string that will seed the "lexi magic". This is assumed to be public knowledge.
-export const publicString = encodeBase64(randomBytes(PUBLIC_STRING_LENGTH));
+export const publicString = base64.encode(randomBytes(PUBLIC_STRING_LENGTH));
 console.log("THE PUBLIC STRING IS: " + publicString);
 
 /**
  * The "normal" way to generate a symmetric key pair
  */
-export const generateKey = () => encodeBase64(randomBytes(secretbox.keyLength));
+export const generateKey = () => base64.encode(randomBytes(secretbox.keyLength));
 
 export class SignWalletWithKey implements SignWallet {
   private signKey: SignKeyPair;
@@ -39,10 +38,9 @@ export class SignWalletWithKey implements SignWallet {
  * @param signer
  */
 export const generateKeyFromSignature = async (publicString: string, signer: SignWallet): Promise<Uint8Array> => {
-  // Sign the non-secret public string
-  const secret = await signer.signMessage(
-    decodeBase64(publicString)
-  );
+  // Hash and sign the non-secret public string
+  const publicHash = crypto.createHash("sha256").update(publicString).digest();
+  const secret = await signer.signMessage(publicHash);
 
   // Hash the signature to standardise it to 32 bytes
   // using tweetnacl for hashing creates a 64 byte hash, so we use the native crypto lib here instead
