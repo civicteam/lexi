d85e4ae52c9b4fd3650706fdd6958dccf14b7732
Frits Stegmann <frits@double-eye.com>
2022-05-17 15:56:59 +0200

    CM-337 Derive the encryption key on connection.
    add mocha tests to the project.

--- packages/lexi/test/index.ts
+++ packages/lexi/test/index.ts
@@ -3,22 +3,23 @@ import { encode } from "bs58";
 import { SignWalletWithKey } from "../src/lib/key";
 import { LexiWallet } from "../src";
 
-(async () => {
-  // Create an asymmetric signing key pair. This mimics the user's crypto wallet
-  const signKey = sign.keyPair();
-  const signer = new SignWalletWithKey(signKey);
+describe("LexiWallet", () => {
+  it("We create a LexiWallet that generates a key when initiated", async () => {
+    const signKey = sign.keyPair();
+    const signer = new SignWalletWithKey(signKey);
 
-  // derive my did from this signing key
-  const me = "did:sol:" + encode(signKey.publicKey);
-  console.log("My DID: " + me);
+    // derive my did from this signing key
+    const me = "did:sol:" + encode(signKey.publicKey);
+    console.log("My DID: " + me);
 
-  // The data we want to encrypt
-  const obj = { hello: "world" };
+    // The data we want to encrypt
+    const obj = { hello: "world" };
 
-  // encrypt and decrypt using lexi-aware wallet
-  const lexiWallet = new LexiWallet(signer, me, {}, true);
-  const encryptedWithWallet = await lexiWallet.encryptForMe(obj);
-  const decrypted = await lexiWallet.decrypt(encryptedWithWallet);
+    // encrypt and decrypt using lexi-aware wallet
+    const lexiWallet = new LexiWallet(signer, me, {}, true);
+    const encryptedWithWallet = await lexiWallet.encryptForMe(obj);
+    const decrypted = await lexiWallet.decrypt(encryptedWithWallet);
 
-  console.log(decrypted, obj); // should be shallow equal
-})().catch(console.error);
+    console.log(decrypted, obj); // should be shallow equal
+  });
+});
