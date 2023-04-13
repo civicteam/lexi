673300ce94b680a500c7e8cc482cf40b90576a68
Christiano Teixeira <chriteixeira@gmail.com>
2023-04-13 00:45:23 -0300

    CPASS-204: Generate new signingString to each wallet

--- packages/lexi/src/index.ts
+++ packages/lexi/src/index.ts
@@ -2,4 +2,4 @@ export { LexiWallet } from "./service/lexi";
 export { SignWallet } from "./lib/wallet";
 export { Resolver, LexiOptions } from "./lib/did";
 export { DIDDocument } from "did-resolver";
-export { singleUsePublicString } from "./lib/key";
+export { generateRandomString } from "./lib/key";
