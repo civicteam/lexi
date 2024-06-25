31076af22e3b6cc445a330bdef818e184fd4be13
dankelleher <dankelleher@yahoo.com>
2024-06-25 16:26:03 +0200

    Add caching inside lexi for signing

--- packages/lexi/src/lib/wallet.ts
+++ packages/lexi/src/lib/wallet.ts
@@ -16,3 +16,22 @@ interface EncryptionWallet {
 export interface PersonalEncryptionWallet extends EncryptionWallet {
   encryptForMe(plaintext: Uint8Array): Promise<EncryptionPackage>;
 }
+
+// A wallet that caches signd messages to avoid multiple calls to the wallet
+export class CachedSignWallet implements SignWallet {
+  private readonly wallet: SignWallet;
+  private readonly cache: Record<string, Uint8Array>;
+
+  constructor(wallet: SignWallet) {
+    this.wallet = wallet;
+    this.cache = {};
+  }
+
+  async signMessage(message: Uint8Array): Promise<Uint8Array> {
+    const key = message.toString();
+    if (!this.cache[key]) {
+      this.cache[key] = await this.wallet.signMessage(message);
+    }
+    return this.cache[key] as Uint8Array;
+  }
+}
